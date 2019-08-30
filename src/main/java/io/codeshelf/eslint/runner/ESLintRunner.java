eab7f12c525ee55c474f56806eb7171407065a2b
Chris Turner <chris@codacy.com>
2019-08-30 10:10:11 +0100

    clean: code formatting

--- src/main/java/io/codeshelf/eslint/runner/ESLintRunner.java
+++ src/main/java/io/codeshelf/eslint/runner/ESLintRunner.java
@@ -2,16 +2,26 @@ package io.codeshelf.eslint.runner;
 
 import io.codeshelf.eslint.service.ProcessService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 
-/** @author Chris Turner (chris@forloop.space) */
+import java.io.IOException;
+
+/**
+ * @author Chris Turner (chris@forloop.space)
+ */
+@Slf4j
 @RequiredArgsConstructor
 public class ESLintRunner implements CommandLineRunner {
 
-  private final ProcessService processService;
+    private final ProcessService processService;
 
-  @Override
-  public void run(final String... strings) throws Exception {
-    processService.execute();
-  }
+    @Override
+    public void run(final String... strings) {
+        try {
+            processService.execute();
+        } catch (IOException | InterruptedException e) {
+            log.error("something went wrong... {}", e.getMessage());
+        }
+    }
 }
