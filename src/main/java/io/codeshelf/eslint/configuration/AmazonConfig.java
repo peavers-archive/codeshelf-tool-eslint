dabbf006a1fd802afc3c473ae03f175f9ba3e963
Chris Turner <chris@codacy.com>
2019-09-03 15:46:31 +0100

    feature: flag to disable pushing to AWS for debugging/dry runs

--- src/main/java/io/codeshelf/eslint/configuration/AmazonConfig.java
+++ src/main/java/io/codeshelf/eslint/configuration/AmazonConfig.java
@@ -1,33 +1,19 @@
 package io.codeshelf.eslint.configuration;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehose;
 import com.amazonaws.services.kinesisfirehose.AmazonKinesisFirehoseClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/** @author Chris Turner (chris@forloop.space) */
+/**
+ * @author Chris Turner (chris@forloop.space)
+ */
 @Configuration
 public class AmazonConfig {
 
-  @Value("${amazon.accessKey}")
-  public String accessKey;
+    @Bean
+    public AmazonKinesisFirehose amazonKinesisFirehose() {
 
-  @Value("${amazon.secretKey}")
-  public String secretKey;
-
-  @Value("${amazon.region}")
-  public String region;
-
-  @Bean
-  public AmazonKinesisFirehose amazonKinesisFirehose() {
-
-    return AmazonKinesisFirehoseClientBuilder.standard()
-        .withCredentials(
-            new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
-        .withRegion(region)
-        .build();
-  }
+        return AmazonKinesisFirehoseClientBuilder.defaultClient();
+    }
 }
